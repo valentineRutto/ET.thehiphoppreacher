cc06e281d47370ed2acb7641e6d908aef3c3086c
valentinerutto <rutto28@gmail.com>
2015-10-15 10:18:12 +0300

    changed app name

---
+++ app/src/main/java/com/akoscz/youtube/MyApp.java
@@ -0,0 +1,21 @@
+package com.akoscz.youtube;
+
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+
+import io.fabric.sdk.android.Fabric;
+
+/**
+ * Created by valentine on 10/14/15.
+ */
+public class MyApp extends android.app.Application {
+    // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
+    private static final String TWITTER_KEY = "3PxKipJ0JoTDHaOLEFQ1IbTcm";
+    private static final String TWITTER_SECRET = "hcB48sOQ9qPCMJ2cLpcYhq7G16wx93eDOqNjWfGEG0kMbCLq5B";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+        Fabric.with(this, new com.twitter.sdk.android.Twitter(authConfig));
+    }
+}
